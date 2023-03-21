51248d9f3685b0c415862a091d8311925ba3b60e
ethan.perruzza <ethan.perruzza@epita.fr>
2023-03-21 11:46:23 +0200

    Game Activity Design Created the design of the activity_game

--- app/src/main/java/fi/centria/perruzza/ethan/gettoknowtheworld/MainActivity.kt
+++ app/src/main/java/fi/centria/perruzza/ethan/gettoknowtheworld/MainActivity.kt
@@ -2,16 +2,26 @@ package fi.centria.perruzza.ethan.gettoknowtheworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonPlay: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonPlay = findViewById(R.id.button_play)
+
         val selectCountryHomePage = SelectCountryHomePage()
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add(R.id.fragment_container_view, selectCountryHomePage)
+        }
+
+        buttonPlay.setOnClickListener{
+
         }
     }
 }
---
+++ app/src/main/java/fi/centria/perruzza/ethan/gettoknowtheworld/game/GameActivity.kt
@@ -0,0 +1,12 @@
+package fi.centria.perruzza.ethan.gettoknowtheworld.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fi.centria.perruzza.ethan.gettoknowtheworld.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
